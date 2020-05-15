d6b873c0e4908e8ce2d391f09296dbb72d6b27d0
as564335sa <809885126@qq.com>
2020-05-15 09:32:23 +0800

    test-pr

--- lv_freetype.h
+++ lv_freetype.h
@@ -1,51 +1,16 @@
-/**
- * @file lv_freetype.h
- *
- */
-
-#ifndef LV_FREETYPE_H
-#define LV_FREETYPE_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/*********************
- *      INCLUDES
- *********************/
-#include <stdint.h>
-#include <ft2build.h>
+#ifndef _LV_FONT_TTF_H
+#define _LV_FONT_TTF_H
+#include "ft2build.h"
 #include FT_FREETYPE_H
-#include "lvgl/lvgl.h"
 
+#define USE_CACHE_MANGER	1
 
-
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
 typedef struct {
     FT_Face     face;      /* handle to face object */
-
+    uint16_t font_size;
 }lv_font_fmt_freetype_dsc_t;
 
+int lv_freetype_init(uint8_t max_font_manger);
+int lv_freetype_font_init(lv_font_t * font, const char * font_path, uint16_t font_size);
 
-
-/**********************
- * GLOBAL PROTOTYPES
- **********************/
-int lv_freetype_init(void);
-int lv_freetype_font_init(lv_font_t * font, const char * font_path, uint16_t size);
-
-/**********************
- *      MACROS
- **********************/
-
-#ifdef __cplusplus
-} /* extern "C" */
 #endif
-
-#endif /*LV_FREETYPE_H*/
