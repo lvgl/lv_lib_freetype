e776a804676ced72017c2ae5cf59a00526e2133d
guowei15 <guowei15@xiaomi.com>
2021-04-06 16:20:58 +0800

    refactor the code

--- lv_freetype.h
+++ lv_freetype.h
@@ -17,27 +17,77 @@ extern "C" {
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 #include FT_CACHE_H
-
-#define USE_CACHE_MANGER	1
+#include FT_SIZES_H
 
 /*********************
  *      DEFINES
  *********************/
+#define LV_USE_FT_CACHE_MANAGER  1
 
 /**********************
  *      TYPEDEFS
  **********************/
- 
 typedef struct {
-    FT_Face     face;      /* handle to face object */
-    uint16_t font_size;		/*font height size */
+    uint16_t cnt;
+    char*   name;
+} lv_face_info_t;
+
+typedef struct {
+    uint16_t num;
+    uint16_t cnt;
+    lv_ll_t  face_ll;
+} lv_faces_control_t;
+
+typedef enum {
+    FT_FONT_STYLE_NORMAL = 0,
+    FT_FONT_STYLE_ITALIC = 1 << 0,
+    FT_FONT_STYLE_BOLD   = 1 << 1
+} LV_FT_FONT_STYLE;
+
+typedef struct {
+    const char* name;   /* The name of the font file */
+    lv_font_t* font;    /* point to lvgl font */
+    uint16_t weight;    /* font size */
+    uint16_t style;     /* font style */
+} lv_ft_info_t;
+
+typedef struct {
+    FT_Face     face;
+    FT_Size     size;
+    lv_font_t*  font;
+    uint16_t    style;
+    uint16_t weight;
 }lv_font_fmt_freetype_dsc_t;
+
+typedef lv_font_fmt_freetype_dsc_t lv_font_fmt_ft_dsc_t;
 
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
-int lv_freetype_init(uint8_t max_faces);
-int lv_freetype_font_init(lv_font_t * font, const char * font_path, uint16_t font_size);
+
+/**
+ * init freetype library
+ * @param max_faces Maximum number of opened FT_Face objects managed by this cache instance. Use 0 for defaults.
+ * @param max_sizes Maximum number of opened FT_Size objects managed by this cache instance. Use 0 for defaults.
+ * @param max_bytes Maximum number of bytes to use for cached data nodes. Use 0 for defaults. 
+ *                  Note that this value does not account for managed FT_Face and FT_Size objects.
+ * @return true on success, otherwise false.
+ */
+bool lv_freetype_init(FT_UInt max_faces, FT_UInt max_sizes, FT_ULong max_bytes);
+
+/**
+ * Creates a font with info parameter specified.
+ * @param info See lv_ft_info_t for details.
+ *             when success, lv_ft_info_t->font point to the font you created.
+ * @return true on success, otherwise false.
+ */
+bool lv_ft_font_init(lv_ft_info_t *info);
+
+/**
+ * Destroy a font that has been created.
+ * @param font pointer to font.
+ */
+void lv_ft_font_destroy(lv_font_t* font);
 
 /**********************
  *      MACROS
