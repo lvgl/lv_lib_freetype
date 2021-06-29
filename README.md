dc90cc7dc792d805de7a0ed4ae1a9abdd1fca160
guowei15 <guowei15@xiaomi.com>
2021-06-29 14:16:42 +0800

    fix cache bug and code format

--- README.md
+++ README.md
@@ -15,28 +15,27 @@ Interface to FreeType to generate font bitmaps run time
 To enable cache, set`LV_USE_FT_CACHE_MANAGER  1`in lv_freetype.h.
 
 ```c
-/* init freetype library */
-lv_freetype_init(64, 1, 0);
+  /*Init freetype library
+   *Cache max 64 faces and 1 size*/
+  lv_freetype_init(64, 1, 0);
 
-/*Create a font*/
-lv_ft_info_t info;
-info.name = "./lv_lib_freetype/arial.ttf"";
-info.size = 16;
-info.style = FT_FONT_STYLE_NORMAL;
-lv_ft_font_init(&info);
+  /*Create a font*/
+  static lv_ft_info_t info;
+  info.name = "./lv_lib_freetype/arial.ttf";
+  info.weight = 16;
+  info.style = FT_FONT_STYLE_NORMAL;
+  lv_ft_font_init(&info);
 
-/*Create style with the new font*/
-static lv_style_t style;
-lv_style_init(&style);
-lv_style_set_text_font(&style, LV_STATE_DEFAULT, info.font);
+  /*Create style with the new font*/
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_text_font(&style, info.font);
 
-/*Create a label with the new style*/
-lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
-lv_obj_add_style(label, LV_LABEL_PART_MAIN, &style);
-lv_label_set_text(label, "Hello world");
+  /*Create a label with the new style*/
+  lv_obj_t * label = lv_label_create(lv_scr_act());
+  lv_obj_add_style(label, &style, 0);
+  lv_label_set_text(label, "Hello world");
 
-/* free font */
-//lv_ft_font_destroy(info.font);
 ```
 
 ## Learn more
--- lv_freetype.h
+++ lv_freetype.h
@@ -2,8 +2,8 @@
  * @file lv_freetype.h
  *
  */
-#ifndef _LV_FONT_TTF_H
-#define _LV_FONT_TTF_H
+#ifndef LV_FONT_FREETYPE_H
+#define LV_FONT_FREETYPE_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,11 +13,6 @@ extern "C" {
  *      INCLUDES
  *********************/
 #include "lvgl/lvgl.h"
-#include "ft2build.h"
-#include FT_FREETYPE_H
-#include FT_GLYPH_H
-#include FT_CACHE_H
-#include FT_SIZES_H
 
 /*********************
  *      DEFINES
@@ -27,17 +22,6 @@ extern "C" {
 /**********************
  *      TYPEDEFS
  **********************/
-typedef struct {
-    uint16_t cnt;
-    char*   name;
-} lv_face_info_t;
-
-typedef struct {
-    uint16_t num;
-    uint16_t cnt;
-    lv_ll_t  face_ll;
-} lv_faces_control_t;
-
 typedef enum {
     FT_FONT_STYLE_NORMAL = 0,
     FT_FONT_STYLE_ITALIC = 1 << 0,
@@ -45,21 +29,11 @@ typedef enum {
 } LV_FT_FONT_STYLE;
 
 typedef struct {
-    const char* name;   /* The name of the font file */
-    lv_font_t* font;    /* point to lvgl font */
+    const char * name;  /* The name of the font file */
+    lv_font_t * font;   /* point to lvgl font */
     uint16_t weight;    /* font size */
     uint16_t style;     /* font style */
 } lv_ft_info_t;
-
-typedef struct {
-    FT_Face     face;
-    FT_Size     size;
-    lv_font_t*  font;
-    uint16_t    style;
-    uint16_t weight;
-} lv_font_fmt_freetype_dsc_t;
-
-typedef lv_font_fmt_freetype_dsc_t lv_font_fmt_ft_dsc_t;
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -73,7 +47,7 @@ typedef lv_font_fmt_freetype_dsc_t lv_font_fmt_ft_dsc_t;
  *                  Note that this value does not account for managed FT_Face and FT_Size objects.
  * @return true on success, otherwise false.
  */
-bool lv_freetype_init(FT_UInt max_faces, FT_UInt max_sizes, FT_ULong max_bytes);
+bool lv_freetype_init(uint16_t max_faces, uint16_t max_sizes, uint32_t max_bytes);
 
 /**
  * Destroy freetype library
@@ -86,13 +60,13 @@ void lv_freetype_destroy(void);
  *             when success, lv_ft_info_t->font point to the font you created.
  * @return true on success, otherwise false.
  */
-bool lv_ft_font_init(lv_ft_info_t *info);
+bool lv_ft_font_init(lv_ft_info_t * info);
 
 /**
  * Destroy a font that has been created.
  * @param font pointer to font.
  */
-void lv_ft_font_destroy(lv_font_t* font);
+void lv_ft_font_destroy(lv_font_t * font);
 
 /**********************
  *      MACROS
@@ -102,4 +76,4 @@ void lv_ft_font_destroy(lv_font_t* font);
 } /* extern "C" */
 #endif
 
-#endif
+#endif /* LV_FONT_FREETYPE_H */
